f632c6815b22e07e29b68aaec44e38599090bc80
Hideki Noma <noma@logitoy.jp>
2019-01-28 15:19:26 +0900

    パラメータ追加（Delay、メッセージなど）

--- wp-simple-noref-redirects.php
+++ wp-simple-noref-redirects.php
@@ -85,7 +85,9 @@ if (!class_exists("SimpleNorefRirects")) {
                         <thead>
                             <tr>
                                 <th colspan="2">Request</th>
-                                <th colspan="2">Destination</th>
+                                <th colspan="1">Destination</th>
+                                <th colspan="1">Delay</th>
+                                <th colspan="2">Original Message</th>
                             </tr>
                         </thead>
                         <tbody>
@@ -95,9 +97,11 @@ if (!class_exists("SimpleNorefRirects")) {
                             </tr>
                             <?php echo $this->expand_redirects(); ?>
                             <tr>
-                                <td style="width:35%;"><input type="text" name="noref_redirects[request][]" value="" style="width:99%;" /></td>
+                                <td style="width:30%;"><input type="text" name="noref_redirects[request][]" value="" style="width:99%;" /></td>
                                 <td style="width:2%;">&raquo;</td>
-                                <td style="width:60%;"><input type="text" name="noref_redirects[destination][]" value="" style="width:99%;" /></td>
+                                <td style="width:30%;"><input type="text" name="noref_redirects[destination][]" value="" style="width:99%;" /></td>
+                                <td style="width:10%;"><input type="text" name="noref_redirects[delay][]" value="" style="width:99%;" /></td>
+                                <td style="width:25%;"><textarea type="text" name="noref_redirects[message][]" style="width:99%;" /></textarea></td>
                                 <td><span class="wpsnoref-delete">Delete</span></td>
                             </tr>
                         </tbody>
@@ -154,13 +158,19 @@ if (!class_exists("SimpleNorefRirects")) {
             $redirects = get_option('noref_redirects');
             $output = '';
             if (!empty($redirects)) {
-                foreach ($redirects as $request => $destination) {
+                foreach ($redirects as $request => $data) {
+                    $setting = unserialize($data);
+                    $destination = $setting['destination'];
+                    $delay = $setting['delay'];
+                    $message = htmlentities($setting['message']);
                     $output .= '
 
 					<tr>
 						<td><input type="text" name="noref_redirects[request][]" value="' . $request . '" style="width:99%" /></td>
 						<td>&raquo;</td>
 						<td><input type="text" name="noref_redirects[destination][]" value="' . $destination . '" style="width:99%;" /></td>
+						<td><input type="text" name="noref_redirects[delay][]" value="' . $delay . '" style="width:99%;" /></td>
+                                                <td><textarea type="text" name="noref_redirects[message][]" style="width:99%;" />' . $message . '</textarea></td>
 						<td><span class="wpsnoref-delete"></span></td>
 					</tr>
 
@@ -190,11 +200,16 @@ if (!class_exists("SimpleNorefRirects")) {
             for ($i = 0; $i < sizeof($data['request']); ++$i) {
                 $request = trim(sanitize_text_field($data['request'][$i]));
                 $destination = trim(sanitize_text_field($data['destination'][$i]));
-
+                $delay = trim(sanitize_text_field($data['delay'][$i]));
+                $message = trim($data['message'][$i]);
                 if ($request == '' && $destination == '') {
                     continue;
                 } else {
-                    $redirects[$request] = $destination;
+                    $redirects[$request] = serialize(array(
+                        'destination' => $destination,
+                        'delay' => $delay,
+                        'message' => $message
+                    ));
                 }
             }
 
@@ -216,23 +231,37 @@ if (!class_exists("SimpleNorefRirects")) {
          */
         function redirect() {
             // this is what the user asked for (strip out home portion, case insensitive)
-            $userrequest = str_ireplace(get_option('home'), '', $this->get_address());
-            $userrequest = rtrim($userrequest, '/');
-
+            $userrequest = rtrim(str_ireplace(get_option('home'), '', $this->get_address()), '/');
             $redirects = get_option('noref_redirects');
             if (!empty($redirects)) {
-
                 $wildcard = get_option('noref_redirects_wildcard');
                 $do_redirect = '';
 
                 // compare user request to each noref stored in the db
-                foreach ($redirects as $storedrequest => $destination) {
+                foreach ($redirects as $storedrequest => $data) {
+                    $setting = unserialize($data);
+                    $destination = $setting['destination'];
+                    $delay = $setting['delay'];
+                    $message = $setting['message'];
+                    if (isset($_GET['ar'])){
+                        $force_refresh = true;
+                        if ($delay < 0){
+                            $delay = 0;
+                        }
+                    }
+                    else {
+                        $force_refresh = false;
+                    }
                     // check if we should use regex search
+                    $canonical_url = $userrequest;
                     if ($wildcard === 'true' && strpos($storedrequest, '*') !== false) {
                         // wildcard redirect
                         // don't allow people to accidentally lock themselves out of admin
                         if (strpos($userrequest, '/wp-login') !== 0 && strpos($userrequest, '/wp-admin') !== 0) {
                             // Make sure it gets all the proper decoding and rtrim action
+                            $storedrequest = str_replace('\\', '\\\\', $storedrequest);
+                            $storedrequest = str_replace('.', '\\.', $storedrequest);
+                            $storedrequest = str_replace('?', '\\?', $storedrequest);
                             $storedrequest = str_replace('*', '(.*)', $storedrequest);
                             $pattern = '/^' . str_replace('/', '\/', rtrim($storedrequest, '/')) . '/';
                             $destination = str_replace('*', '$1', $destination);
@@ -250,22 +279,27 @@ if (!class_exists("SimpleNorefRirects")) {
                     // redirect. the second condition here prevents redirect loops as a result of wildcards.
                     if ($do_redirect !== '' && trim($do_redirect, '/') !== trim($userrequest, '/')) {
                         // check if destination needs the domain prepended
+
+                        header('Referrer-Policy: no-referrer');
                         if (strpos($do_redirect, '/') === 0) {
                             $do_redirect = home_url() . $do_redirect;
                         }
                         $do_header_refresh = false;
+                        $meta_noref = 'no-referrer';
                         if (strpos('MSIE', $_SERVER['HTTP_USER_AGENT']) !== false) {
                             $do_header_refresh = true;
+                            $meta_noref = 'never'; // IE11までとEdge
                         }
                         if ($do_header_refresh == true) {
-                            header('Refresh:0;URL=' . $do_redirect);
+                            // TODO 要検証 header('Refresh:0;URL=' . $do_redirect);
                         }
+
                         ?>
                         <!doctype html>
                         <html lang="ja">
                             <head>
                                 <meta charset="utf-8" />
-                                <meta name="referrer" content="no-referrer" />
+                                <meta name="referrer" content="<?php echo $meta_noref; ?>" />
                                 <script>
 
                                     function check_ie() {
@@ -300,15 +334,23 @@ if (!class_exists("SimpleNorefRirects")) {
 
 
                                     if (check_ie()) {
-                                        location.href = "<?php echo $do_redirect; ?>";
+                                        <?php if ($delay >= 0): ?>
+                                        setTimeout(function(){ location.href = "<?php echo $do_redirect; ?>"; }, <?php echo $delay * 1000 + 1; ?>);
+                                        <?php endif; ?>
                                     }
                                 </script>
-                                <link rel="canonical" href="<?php echo $do_redirect; ?>" />
-                                <meta http-equiv="refresh" content="0; URL=<?php echo $do_redirect; ?>" />
+                                <link rel="canonical" href="<?php echo $canonical_url; ?>" />
+                                <?php if ($delay >= 0): ?>
+                                <meta http-equiv="refresh" content="<?php echo $delay; ?>; URL=<?php echo $do_redirect; ?>" />
+                                <?php endif; ?>
                             </head>
                             <body>
+                                <?php if ($message != ''): ?>
+                                <?php echo $message; ?>
+                                <?php else: ?>
                                 <h1>画面切り替え中・・・</h1>
                                 <p>画面が切り替わらない場合は <a href="<?php echo $do_redirect; ?>" rel="noreferrer">こちらのリンク</a>をクリックしてください。</p>
+                                <?php endif; ?>
                             </body>
                         </html>
                         <?php
